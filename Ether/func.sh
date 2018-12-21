333e4913baf535ec0afa6e0b2bb38f1a132f1604
DaWe HP <willteam35@gmail.com>
2018-12-21 22:36:18 +0100

    eth functions

--- Ether/func.sh
+++ Ether/func.sh
@@ -7,10 +7,23 @@ function getblocknumber() {
 }
 
 function getbalance() {
-    local  balance=$(curl --data '{"method":"eth_getBalance","params":["0xb9515e8dba64827c96bb7e324374ed4e24878dc1"],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545)
+    local  balance=$(curl --data '{"method":"eth_getBalance","params":["0xf4d9ac9b0363e0ef8a8b78f16fc35a0da044c0c9"],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545)
     hexbal="$(jq '.result' <<< $balance)"
     bc <<< "scale = 8; $decbal * 0.000000000000000001"
 }
 
 getbalance=$(getbalance)
 echo $getbalance
+
+function getStoragePercent() { # in GB
+    df --output=pcent / | tr -dc '0-9'
+}
+function getStorageUsed() {
+    df -h --output=used / | tr -dc '0-9'
+}
+function getStorageAvail() {
+    df -h --output=avail / | tr -dc '0-9'
+}
+function getStorageSize() {
+    df -h --output=size / | tr -dc '0-9'
+}
