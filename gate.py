dd61b559861aac7f07ad71cbd2e150b55dcb7370
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-03-06 16:20:36 +0100

    pseudo logic

--- gate.py
+++ gate.py
@@ -1,9 +1,49 @@
+import schedule
 import config
+from filedb.filedb import Filedb
 
 package = 'currencies.' + config.NODE
 Node = getattr(__import__(package, fromlist=['Node']), 'Node')
 
+package = 'protocols.' + config.PROTOCOL + '.rest'
+Node = getattr(__import__(package, fromlist=['Protocol']), 'Protocol')
+
 node = Node()
+protocol = Protocol()
+
+walletnotify_txt = Filedb('walletnotify')
+walletnotify_lastcheck = 0
+
+lastblockhash_txt = Filedb('lastblockhash')
+lastblockhash_lastcheck = 0
+
+
+# withdraw & generate new addresses every 10 min
+def send_gen():
+    fetch from exchane
+    
+    newaddresses = []
+    while fetch.needNewAddres < newaddresses.len():
+        newaddresses[] = node.newaddress()
+    send new addresses
+
+    foreach fetch.withdraw as withdraw
+        filedb.push(withdraw.id)
+        node.send(withdraw.address)
+
+schedule.every(1).minute.do(send_gen)
+
+while True:
+    if lastblockhash_txt.lastModify() > lastblockhash_lastcheck:
+        fetch from exchane
+        update required tx
+    if walletnotify_txt.lastModify() > walletnotify_lastcheck:
+        send new tx
+
+    schedule.run_pending()
+
+
+
 print('Balance:', node.balance(), 'BTC')
 print('Blockcount:', node.blockcount())
 # print('GetTransaction:', node.gettransaction('624f894301f2be2b4ab09dc0b42891550a1ea0dbb651a4c3287f4e5332446571'))
