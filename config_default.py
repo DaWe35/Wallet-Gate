098940a5a73595750f82f135ceb281689d51fae9
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-03-06 16:20:23 +0100

    protocol config

--- config_default.py
+++ config_default.py
@@ -2,3 +2,7 @@ NODE = ''   # __example_implementation__, bitcoin, ethereum, etc. files from cur
 PRC_USER = ''
 RPC_PASSWORD = ''
 RPC_PORT = ''   # 8332 for Bitcoin, 18332 for BTC testnet
+
+PROTOCOL = 'rest'   # rest or ws
+STREAM_URL = 'protocols/rest/server/'    # https://your.wallet/stream/
+STREAM_PASSWORD = ''
